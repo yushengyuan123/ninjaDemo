57bf7466453daf9943c945acee5cba5c73655834
yushengyuan123 <1749238278@qq.com>
2022-01-28 10:11:53 +0800

    change

---
+++ src/test.c
@@ -0,0 +1,11 @@
+//
+// Created by 余圣源 on 2022/1/28.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("hello, world form test1");
+	return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 余圣源 on 2022/1/28.
+//
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello, world" << std::endl;
+	return 0;
+}
